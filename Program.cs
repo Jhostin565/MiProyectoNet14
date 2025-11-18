05788ad5d3c05bdc33c03f02ef3ae5f20c5f11fc
Jhostin565 <yaguno2028@gmail.com>
2025-11-18 16:03:05 -0500

    Prueba de Render

--- Program.cs
+++ Program.cs
@@ -14,5 +14,5 @@ app.MapGet("/", () => "Hola desde mi API en Render!");
 
 // Ruta de Health Check (Render la usa para ver si tu app está viva)
 app.MapGet("/healthz", () => "OK");
-
+//ayuda
 app.Run();
